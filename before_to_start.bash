7b479a97e960075856c62a077a9f19878329571a
Matthieu <matt.doff@gmail.com>
2022-02-15 16:56:58 +0100

    New version, overall revision

--- before_to_start.bash
+++ before_to_start.bash
@@ -1,45 +1,58 @@
 #!/bin/bash
 
 # description
-# before to start from clone or template
-# last update : 2021 08 18
-# version number : 1
+# Initialisation before to start from a clone or a template
 
 #-----------------------#
 #	Functions	#
 #-----------------------#
 
-change_the_hostname () {
-	# on recupere le hostname a changer
-	old=$(hostname)
+update () {
 
-	# modifie le hostname par celui ecrit par l'utilisateur
-	read -p 'your new hostname : ' hostname
-	hostnamectl set-hostname $hostname
+# 	Update packages
+	sudo apt update 
+	sudo apt upgrade -y
+	sudo apt autoremove -y
 
-	# modifie le "hostname.home" et le "hostname" a la deuxième ligne
-	grep -w $old /etc/hosts |  sed -i "2s/$old/$hostname/g" /etc/hosts # grep cherche l'ancien hostname et sed remplace par le nouveau
+# 	Installating VMware tools & sudo
+	sudo apt install open-vm-tools sudo -y
 }
 
-create_an_user () {
-	read -p 'write the OLD login name : ' old_user
-	read -p 'write the NEW login name : ' new_user
+change_the_hostname () {
+#	Get old hostname
+	old=$(hostname)
 
-	# creation du nouvel utilisateur et son /home
-	# useradd is native binary compiled with the system. But, adduser is a perl script which uses useradd binary in back-end
-	useradd $new_user --home /home/$new_user/ --create-home --groups sudo --shell /bin/bash
+#	New hostname fulfilled by user
+	read -p 'your new hostname : ' hostname
+	sudo hostnamectl set-hostname $hostname
 
-	# mot de passe pour le nouvel utilisateur
-	passwd $new_user
-
-	# suppression de l'ancien utilisateur
-	pkill -9 -u $old_user && userdel --remove $old_user
+#	Modifying "hostname.home" and "hostname" at the second line
+	grep -w $old /etc/hosts | sudo sed -i "2s/$old/$hostname/g" /etc/hosts
 }
 
 check_net_int_conf_file () {
-	grep -quiet allow-hotplug /etc/network/interfaces 2> /dev/null
+#	For Debian
+	grep -w allow-hotplug /etc/network/interfaces 2> /dev/null
 	if	[[ $(echo $?) -eq 0 ]] ; then
-		sed -i 's/allow-hotplug/auto/' /etc/network/interfaces
+		sudo sed -i 's/allow-hotplug/auto/' /etc/network/interfaces
+	fi
+}
+
+create_an_user () {
+	read -p 'write your NEW username : ' username
+	read -ps 'write your NEW password : ' password
+
+#	Creating new user and /home
+# Info:	useradd is native binary compiled with the system / adduser is a perl script which uses useradd binary in back-end
+	sudo useradd $username --create-home --home /home/$username/ --groups sudo --shell /bin/bash --password $password
+}
+
+#	pwd_root () {
+	if !	[[ $UID -eq 0 || $(pwd) == "/root" ]] ; then 
+		echo "Run 'su root' & 'mv $0 -t /root/'"
+		echo "And then"
+		echo "Run 'su - root' ; $0'"
+		exit 1
 	fi
 }
 
@@ -47,27 +60,16 @@ check_net_int_conf_file () {
 #	Start	    #
 #-------------------#
 
-# si ce n est pas root OU qu on n est pas dans /root
-if	[[ $UID -ne 0 || $(pwd) != "/root" ]] ; then 
-		echo "Run 'su root' & 'mv $0 -t /root/'"
-		echo "And then"
-		echo "Run 'su - root' ; $0'"
-		exit 1
-fi
-
-# update packages
-apt update && apt -y upgrade && apt -y autoremove
-
-# VMware tools & sudo
-apt -y install open-vm-tools sudo
+update
 
 change_the_hostname
 
-passwd root
+echo "Root password"
+sudo passwd root
 
 check_net_int_conf_file
 
-# message
-echo "shut the VM down & feel free to make a snapshot before going further !" ; sleep 5
-
 create_an_user
+
+#	Message
+echo "'logout' to use your new login : $username"
