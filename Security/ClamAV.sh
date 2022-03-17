256fb6724e2181ab5e5c3fdb9824e9d33ad0bf16
Matthieu <matt.doff@gmail.com>
2022-03-17 16:48:04 +0100

    Created ClamAV

---
+++ Security/ClamAV.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+#-----------------------#
+#	Variables	#
+#-----------------------#
+
+include_path=(
+"/tome"
+)
+
+#-----------------------#
+#	Functions	#
+#-----------------------#
+
+clamd_conf ()
+{
+	for i in ${include_path[@]}
+	do
+		echo "OnAccessIncludePath $i" | sudo tee -a /etc/clamav/clamd.conf > /dev/null
+	done
+
+	echo "OnAccessExcludeUname clamav" | sudo tee -a /etc/clamav/clamd.conf > /dev/null
+	echo "OnAccessPrevention yes" | sudo tee -a /etc/clamav/clamd.conf > /dev/null
+	echo "OnAccessDisableDDD yes" | sudo tee -a /etc/clamav/clamd.conf > /dev/null
+
+	sudo sed -i "s/LocalSocketGroup clamav/LocalSocketGroup root/" /etc/clamav/clamd.conf
+	sudo sed -i "s/User clamav/User root/" /etc/clamav/clamd.conf
+}
+
+clamonacc () 
+{
+	sudo touch /etc/systemd/system/clamonacc.service
+
+	echo "[Unit]" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+	echo "Description=ClamAV On Access Scanner" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+	echo "Requires=clamav-daemon.service" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+	echo "After=clamav-daemon.service" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+	echo "After=syslog.target" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+	echo "After=network-online.target" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+	echo "" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+	echo "[Service]" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+	echo "Type=simple" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+	echo "User=root" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+	echo "ExecStartPre=/bin/bash -c "while [ ! -S /var/run/clamav/clamd.ctl ]; do sleep 1; done"" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+	echo "ExecStart=/usr/sbin/clamonacc -F --fdpass --config-file=/etc/clamav/clamd.conf --log=/var/log/clamav/clamonacc.log" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+	echo "" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+	echo "[Install]" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+	echo "WantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/clamonacc.service > /dev/null
+
+	sudo chmod 644 /etc/systemd/system/clamonacc.service
+	sudo systemctl enable --now clamonacc
+}
+
+#-------------------#
+#	Start	    #
+#-------------------#
+
+sudo apt update
+sudo apt install clamav clamav-daemon
+
+sudo service clamav-freshclam stop
+sudo freshclam
+sudo service clamav-freshclam start
+
+sudo systemctl start clamav-daemon
+
+clamd_conf
+
+clamonacc
+
+sudo systemctl restart clamav-daemon
+
+echo "Scan test" && sleep 3
+sudo apt install curl
+curl https://www.eicar.org/download/eicar.com.txt | clamdscan -
