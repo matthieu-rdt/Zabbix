95cee35309ef43e68104bd2b9ae94bb781e410a8
Matthieu <matt.doff@gmail.com>
2022-04-11 14:56:38 +0200

    Update URL and dpkg

--- Installation/installation_or_upgrade_zabbix_agent_linux.sh
+++ Installation/installation_or_upgrade_zabbix_agent_linux.sh
@@ -36,7 +36,7 @@ case $3 in
 	;;
 
 	"6.0")
-		curl -O "https://repo.zabbix.com/zabbix/6.0/$2/pool/main/z/zabbix-release/zabbix-release_6.0-1+$(lsb_release -sc)_all.deb"
+		curl -O "https://repo.zabbix.com/zabbix/6.0/$2/pool/main/z/zabbix-release/zabbix-release_6.0-1+$2$(lsb_release -sr)_all.deb"
 		sudo dpkg -i zabbix-release_6.0-1+$2$(lsb_release -sr)_all.deb
 	;;
 
