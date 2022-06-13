ca55e14d77ac2b56fd1fad45730b9cb8b60d5077
Matthieu <matt.doff@gmail.com>
2022-06-13 18:15:00 +0200

    Edit last sed

--- Database/galera_cluster_configuration.sh
+++ Database/galera_cluster_configuration.sh
@@ -91,7 +91,9 @@ mariadb_server_cnf $1
 
 ConfirmChoice "Is it the first node of the cluster ?" && sudo galera_new_cluster
 
-echo "You can copy/paste this line after configuring all the nodes :"
-echo "sudo sed -i \"s|\"gcomm://\"|\"gcomm://$ip_node_1,$ip_node_2\"|\" /etc/mysql/mariadb.conf.d/50-server.cnf"
-
 sudo systemctl restart mariadb.service
+
+sudo sed -i \"s|\"gcomm://\"|\"gcomm://$ip_node_1,$ip_node_2\"|\" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+echo "You can restart MariaDB service"
+echo "sudo systemctl restart mariadb.service"
