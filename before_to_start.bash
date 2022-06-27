12bcb1d2eb8c8283348d5b3601a087a4470aab3d
Matthieu <matt.doff@gmail.com>
2022-06-27 11:11:32 +0200

    Enabled pwd_root

--- before_to_start.bash
+++ before_to_start.bash
@@ -9,49 +9,50 @@
 
 update () {
 
-# 	Update packages
-	sudo apt update 
-	sudo apt upgrade -y
-	sudo apt autoremove -y
+	echo "Update packages"
+	apt update
+	apt upgrade -y
+	apt autoremove -y
 
-# 	Installating VMware tools & sudo
-	sudo apt install open-vm-tools sudo -y
+	echo "Installating VMware tools & sudo"
+	apt install open-vm-tools sudo -y
 }
 
 create_an_user () {
 	read -p 'write your NEW username : ' username
 
 #	Creating new user and /home
-# Info:	useradd is native binary compiled with the system / adduser is a perl script which uses useradd binary in back-end
-	sudo useradd $username --create-home --home /home/$username --groups sudo --shell /bin/bash
+# 	Info:	useradd is native binary compiled with the system / adduser is a perl script which uses useradd binary in back-end
+	useradd $username --create-home --home /home/$username --groups sudo --shell /bin/bash
 
 #	Creating new user's password
-	sudo passwd $username
+	passwd $username
 }
 
-change_the_hostname () {
+change_hostname () {
 #	Get old hostname
 	old=$(hostname)
 
 #	New hostname fulfilled by user
 	read -p 'your new hostname : ' hostname
-	sudo hostnamectl set-hostname $hostname
+	hostnamectl set-hostname $hostname
 
 #	Modifying "hostname.domain" and "hostname" at the second line
-	grep -w $old /etc/hosts | sudo sed -i "2s/$old/$hostname/g" /etc/hosts
+	grep -w $old /etc/hosts | sed -i "2s/$old/$hostname/g" /etc/hosts
 }
 
 check_net_int_conf_file () {
 #	For Debian
-	grep -quiet allow-hotplug /etc/network/interfaces 2> /dev/null
-	if	[[ $(echo $?) -eq 0 ]] ; then
-		sudo sed -i 's/allow-hotplug/auto/' /etc/network/interfaces
+	if [ $(cat /etc/os-release | grep -w ID | cut -d "=" -f2) == debian ] ; then
+		grep -quiet allow-hotplug /etc/network/interfaces 2> /dev/null
+		if	[[ $(echo $?) -eq 0 ]] ; then
+			sed -i 's/allow-hotplug/auto/' /etc/network/interfaces
+		fi
 	fi
 }
 
-# Inactive
 pwd_root () {
-	if !	[[ $UID -eq 0 || $(pwd) == "/root" ]] ; then 
+	if 	[[ $UID -ne 0 || $(pwd) != "/root" ]] ; then
 		echo "Run 'su root' & 'mv $0 -t /root/'"
 		echo "And then"
 		echo "Run 'su - root' ; $0'"
@@ -63,14 +64,16 @@ pwd_root () {
 #	Start	    #
 #-------------------#
 
+pwd_root
+
 update
 
 create_an_user
 
 echo "Root password :"
-sudo passwd root
+passwd
 
-change_the_hostname
+change_hostname
 
 check_net_int_conf_file
 
