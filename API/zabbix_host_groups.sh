b796c8daaff9d0bc729636422d2d761f8ebd3da6
Matthieu <matt.doff@gmail.com>
2022-07-27 17:11:19 +0200

    Overall revision

--- API/zabbix_host_groups.sh
+++ API/zabbix_host_groups.sh
@@ -7,11 +7,22 @@
 # https://github.com/q1x/zabbix-gnomes
 
 file_add="$1"
-file_del="$2"
+file_delete="$2"
 
 #-----------------------#
 #	Functions	#
 #-----------------------#
+
+# Function from Manu
+function ConfirmChoice ()
+{
+	ConfYorN="";
+	while [ "${ConfYorN}" != "y" -a "${ConfYorN}" != "Y" -a "${ConfYorN}" != "n" -a "${ConfYorN}" != "N" ] ; do
+		echo -n $1 "(y/n) : "
+		read ConfYorN
+	done
+	[ "${ConfYorN}" == "y" -o "${ConfYorN}" == "Y" ] && return 0 || return 1
+}
 
 home ()
 {
@@ -24,10 +35,10 @@ check_args ()
 {
 	if [[ -z $1 ]] ; then
 		echo "Fill in a file if you've got host groups to add !"
-		exit 1
+		exit 2
 		if [[ -z $2 ]] ; then
 			echo "Fill in a file if you've got host groups to delete !"
-			exit 2
+			exit 3
 		fi
 	fi 
 }
@@ -38,34 +49,35 @@ conf_file_exists ()
 		echo ".zbx.conf does not exist & will be created"
 		echo "API credentials must be added inside"
 		touch $HOME/.zbx.conf
+		exit 4
 	fi
 }
 
 create_host_groups ()
 {
-	if [[ -f $HOME/zabbix-gnomes/zgcreate.py ]] ; then
+	if [[ -f $HOME/zgcreate.py ]] ; then
 	#	Python script call
 		while IFS= read -r line
 		do
-			$HOME/zabbix-gnomes/./zgcreate.py "$line"
-		done < "$file_add"
+			$HOME/./zgcreate.py "$line"
+		done < $file_add
 	else
 		echo "zgcreate.py is missing
-		exit 1
+		exit 5
 	fi
 }
 
 delete_host_groups ()
 {
-	if [[ -f $HOME/zabbix-gnomes/zgdelete.py ]] ; then
+	if [[ -f $HOME/zgdelete.py ]] ; then
 	# 	Python script call
 		while IFS= read -r line
 		do
-			$HOME/zabbix-gnomes/./zgdelete.py -N "$line"
-		done < "$file_del"
+			$HOME/./zgdelete.py -N "$line"
+		done < $file_delete
 	else
 		echo "zgdelete.py is missing
-		exit 2
+		exit 55
 	fi
 }
 
@@ -80,18 +92,6 @@ check_args $1 $2
 #	Check API credentials to connect to Zabbix GUI
 conf_file_exists
 
-read -p 'Do you want to create host groups [y or press Enter] ? ' c_hstgrp
+ConfirmChoice "Do you have some host groups to add ?" && create_host_groups || echo "no action"
 
-if [[ $c_hstgrp == y && -n $1 ]] ; then
-	create_host_groups
-else
-	echo "no action"
-fi
-
-read -p 'Do you want to delete host groups [y or press Enter] ? ' d_hstgrp
-
-if [[ $d_hstgrp == y && -n $2 ]] ; then
-	delete_host_groups
-else
-	echo "no action"
-fi 
+ConfirmChoice "Do you have some host groups to delete ?" && delete_host_groups || echo "no action"
