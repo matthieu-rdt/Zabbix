9f581dbc634244aa7b25f4bf74ad8f9ad41c6a34
Matthieu <matt.doff@gmail.com>
2022-07-28 10:06:38 +0200

    First up

--- Database/galera_arbitrator_conf.sh
+++ Database/galera_arbitrator_conf.sh
@@ -2,3 +2,10 @@
 
 # description
 # setup Galera Arbitrator for avoiding split-brain situations
+
+sudo apt install galera-arbitrator-4
+
+#	Configuration file
+sudo vi /etc/default/garb
+
+sudo service garb start
