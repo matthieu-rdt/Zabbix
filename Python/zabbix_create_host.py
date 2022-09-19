fee2db65a38b3b32c5db97cfb44362c7b04ed34d
Matthieu <matt.doff@gmail.com>
2022-09-19 16:59:39 +0200

    Oops, generic info

--- Python/zabbix_create_host.py
+++ Python/zabbix_create_host.py
@@ -6,15 +6,17 @@ from pyzabbix import ZabbixAPI
 from progressbar import ProgressBar, Percentage, ETA, ReverseBar, RotatingMarker, Timer
 import csv
 
-file_exists('/home/test/host_list.csv')
-zapi = ZabbixAPI("http://172.25.160.141/zabbix")
+file_exists('/path/to/file.csv')
+zapi = ZabbixAPI("http://1.2.3.4/zabbix")
+
+# Credentials by default
 zapi.login(user="Admin", password="zabbix")
 
-arq = csv.reader(open('/home/test/host_list.csv'))
+arq = csv.reader(open('/path/to/file.csv'))
 
 lines = sum(1 for line in arq)
 
-f = csv.reader(open('/home/test/host_list.csv'), delimiter=';')
+f = csv.reader(open('/path/to/file.csv'), delimiter=';')
 bar = ProgressBar(maxval=lines, widgets=[
                   Percentage(), ReverseBar(), ETA(), RotatingMarker(), Timer()]).start()
 i = 0
