92fdd28bf8abc21b2840a317ce567fdbaa91bdf9
Matthieu <matt.doff@gmail.com>
2022-09-20 12:09:56 +0200

    Add if conditions

--- API/zabbix_API_login_logout.sh
+++ API/zabbix_API_login_logout.sh
@@ -2,6 +2,7 @@
 
 # Work in progress
 #auth=`cat $filename | sed 's/.*result":"\([a-zA-Z0-9].*\).*/\1/'`
+#sed -i '51s/"auth": ".*"/"auth": "'$token'" }'
 
 filename=`mktemp /tmp/result_XXX_$$`
 
@@ -16,9 +17,16 @@ function ConfirmChoice ()
 	[ "${ConfYorN}" == "y" -o "${ConfYorN}" == "Y" ] && return 0 || return 1
 }
 
-read -p 'Your Zabbix username ' user
-read -sp 'Your Zabbix password ' password
-read -p 'Your Zabbix URL (example : 192.181.1.2) ' IP
+if      [ $1 == login ] ; then
+        read -p 'Your Zabbix username ' user
+        read -sp 'Your Zabbix password ' password
+        read -p 'Your Zabbix URL (example : 192.181.1.2) ' IP
+elif    [ $1 == logout ] ; then
+        read -p 'Your Zabbix URL (example : 192.181.1.2) ' IP
+else
+	echo 'no parameters'
+	exit 2
+fi
 
 # Get "auth" to connect to Zabbix
 login ()
@@ -33,17 +41,17 @@ login ()
 
 login > $filename
 token=`cat $filename | cut -d':' -f3 | cut -d'"' -f2`
-sed -i "46s/jeton/$token/" $0
+sed -i "52s/jeton/$token/" $0
 
 # Logout from Zabbix API
 logout ()
 {
-        curl --header "Content-Type: application/json" --request POST --data '{
-        "jsonrpc": "2.0",
-        "method": "user.logout",
-        "params": {},
-        "id": 2,
-        "auth": "jeton" }' "http://$IP/zabbix/api_jsonrpc.php"
+	curl --header "Content-Type: application/json" --request POST --data '{
+	"jsonrpc": "2.0",
+	"method": "user.logout",
+	"params": {},
+	"id": 2,
+	"auth": "jeton" }' "http://$IP/zabbix/api_jsonrpc.php"
 }
 
 ConfirmChoice "Do you want to connect to Zabbix API" && login
