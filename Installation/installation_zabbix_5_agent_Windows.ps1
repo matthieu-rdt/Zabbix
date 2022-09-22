d62b1f1f8cb3046089f691746e9616e20be138de
Matthieu <matt.doff@gmail.com>
2022-09-22 16:01:53 +0200

    Major update

--- Installation/installation_zabbix_5_agent_Windows.ps1
+++ Installation/installation_zabbix_5_agent_Windows.ps1
@@ -13,46 +13,56 @@ $DirectoryPath = "C:\your\path\to\directory"
 # Set IP address of Zabbix server
 $IpAddr = "1.2.3.4"
 
+# Zabbix agent version filename
+$ZabbixAgentVersion = "zabbix_agent-5.0.28-windows-amd64-openssl"
+
 # Test the path exists
 if ( (Test-Path -Path $DirectoryPath) -eq $false ) 
 {
-	Write-Host 'edit $DirectoryPath, line 12'
-	break
+	Write-Host 'edit $DirectoryPath'
+	exit
 }
 
 # Test the IP exists
 if ( $IpAddr -eq '1.2.3.4' ) 
 {
-	Write-Host "edit $IpAddr, line 14"
-	break
+	Write-Host 'edit $IpAddr'
+	exit
 }
 
-#	Download Zabbix agent on the user's desktop
-Invoke-WebRequest https://www.zabbix.com/downloads/5.0.1/zabbix_agent-5.0.1-windows-amd64-openssl.zip -OutFile "$DirectoryPath\zabbix_agent-5.0.1-windows-amd64-openssl.zip"
-
-#	Unzip Zabbix agent
-Expand-Archive -LiteralPath "$DirectoryPath\zabbix_agent-5.0.1-windows-amd64-openssl.zip" -DestinationPath "$DirectoryPath\" -Confirm:$false
+# Test Zabbix agent exists
+if ( (Test-Path -Path $DirectoryPath\$ZabbixAgentVersion.zip) -eq $false )
+{
+	Write-Host 'edit $ZabbixAgentVersion'
+	exit
+}
 
 #	Create a new directory called "Zabbix"
 New-Item -Path "C:\Program Files\" -Name "Zabbix" -ItemType "directory"
 
+#	Download Zabbix agent on the user's desktop
+Invoke-WebRequest https://cdn.zabbix.com/zabbix/binaries/stable/5.0/5.0.28/$ZabbixAgentVersion.zip -OutFile "$DirectoryPath\$ZabbixAgentVersion.zip"
+
+#	Unzip Zabbix agent
+Expand-Archive -Path "$DirectoryPath\$ZabbixAgentVersion.zip" -DestinationPath "$DirectoryPath" -Confirm:$false
+
 #	Copy file to Zabbix directory
-Copy-Item "$DirectoryPath\zabbix_agent-5.0.1-windows-amd64-openssl\bin\zabbix_agentd.exe" -Destination "C:\Program Files\Zabbix"
-Copy-Item "$DirectoryPath\zabbix_agent-5.0.1-windows-amd64-openssl\conf\zabbix_agentd.conf" -Destination "C:\Program Files\Zabbix"
+#Copy-Item "$DirectoryPath\$ZabbixAgentVersion\bin\zabbix_agentd.exe" -Destination "C:\Program Files\Zabbix"
+#Copy-Item "$DirectoryPath\$ZabbixAgentVersion\conf\zabbix_agentd.conf" -Destination "C:\Program Files\Zabbix"
 
 ##	"Sed" to edit configuration file
 
 #	Set the IP address for Server
-Get-Content "C:\Program Files\Zabbix\zabbix_agentd.conf" | %{$_ -replace "Server=.*", "Server=$IpAddr"} | Set-Content "C:\Program Files\Zabbix\zabbix_agentd.conf"
+Get-Content "$DirectoryPath\$ZabbixAgentVersion\conf\zabbix_agentd.conf" | %{$_ -replace "Server=127.0.0.1", "Server=$IpAddr"} | Set-Content "C:\Program Files\Zabbix\zabbix_agentd.conf"
 
 #	Set the IP address for ServerActive
-Get-Content "C:\Program Files\Zabbix\zabbix_agentd.conf" | %{$_ -replace "ServerActive=.*", "ServerActive=$IpAddr"} | Set-Content "C:\Program Files\Zabbix\zabbix_agentd.conf"
+Get-Content "$DirectoryPath\$ZabbixAgentVersion\conf\zabbix_agentd.conf" | %{$_ -replace "ServerActive=127.0.0.1", "ServerActive=$IpAddr"} | Set-Content "C:\Program Files\Zabbix\zabbix_agentd.conf"
 
 #	Define HostMetadataItem
-Get-Content "C:\Program Files\Zabbix\zabbix_agentd.conf" | %{$_ -replace "# HostMetadataItem=", "HostMetadataItem=system.uname"} | Set-Content "C:\Program Files\Zabbix\zabbix_agentd.conf"
+Get-Content "$DirectoryPath\$ZabbixAgentVersion\conf\zabbix_agentd.conf" | %{$_ -replace '# HostMetadataItem=', "HostMetadataItem=system.uname"} | Set-Content "C:\Program Files\Zabbix\zabbix_agentd.conf"
 
 #	Set up dynamically "Hostname"
-Get-Content "C:\Program Files\Zabbix\zabbix_agentd.conf" | %{$_ -replace "# HostnameItem=system.hostname", "HostnameItem=system.hostname[host]"} | Set-Content "C:\Program Files\Zabbix\zabbix_agentd.conf"
+Get-Content "$DirectoryPath\$ZabbixAgentVersion\conf\zabbix_agentd.conf" | %{$_ -replace '# HostnameItem=system.hostname', "HostnameItem=system.hostname[host]"} | Set-Content "C:\Program Files\Zabbix\zabbix_agentd.conf"
 
 #	Installation
 "C:\Program Files\Zabbix\zabbix_agentd.exe" -c "C:\Program Files\Zabbix\zabbix_agentd.conf" -i
