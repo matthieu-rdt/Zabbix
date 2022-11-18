1975e792cfb630e28c54e095648556adff6259d7
Matthieu <matt.doff@gmail.com>
2022-11-18 19:26:23 +0100

    Major update, created some functions and made some improvements

--- Backup/rsync_configuration_files.sh
+++ Backup/rsync_configuration_files.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
+source "$HOME/sync_files_list.txt"
+
 # Name of the 'Host' in $HOME/.ssh/config
 remote_host=""
 
-sync_important_files=(
-# add a comment
-'path/file/to/save'
-)
+# Path of the script to make some changes after rsync
+script_path=""
 
 #-----------------------#
 #	Functions       #
 #-----------------------#
 
+red_text ()
+{
+	echo -e "\033[0;31m$1\033[0m"
+}
+
+check_ssh_config ()
+{
+	grep -qw 'Host' $HOME/.ssh/config
+	if	[ $? -eq 1 ] || [ $? -eq 2 ] ; then
+		echo 'No such file'
+		echo 'Visit https://linuxize.com/post/using-the-ssh-config-file/ for further information'
+		echo 'Downloading SSH script'
+		curl -sO "https://raw.githubusercontent.com/matthieu-rdt/Toolbox/master/Linux/SSH.sh" && chmod u+x SSH.sh
+		exit 4
+	fi
+}
+
 sync_all_files ()
 {
+	sudo apt list rsync
+	if	[ $? -eq 1 ] ; then
+		echo 'Installing rsync'
+		sudo apt-get install rsync -y
+	fi
+
 	# if $file is a file AND if line does not start with #
 	if [[ -f $file && ! $file =~ ^# ]] ; then
 		rsync -avz --delete $file $remote_host:$file
@@ -24,31 +47,28 @@ sync_all_files ()
 #       Start       #
 #-------------------#
 
-if	[ -z $remote_host ] ; then
-	echo 'fulfil variable remote_host'
+if	[ ! -f "$HOME/sync_files_list.txt" ] ; then
+	echo "Downloading sync_files_list.txt
+	curl -sO "https://raw.githubusercontent.com/matthieu-rdt/Zabbix/main/Backup/sync_files_list.txt"
+	red_text "Open sync_files_list.txt and add the files you want to synchronise"
+	exit 2
+fi
+
+grep -E --quiet '=""$' $0
+if	[ $? -eq 0 ] ; then
+	echo 'fulfil variables'
 	exit 3
 fi
 
-grep -qw 'Host' $HOME/.ssh/config
-if	[ $? -eq 1 ] ; then
-	echo 'No shortcuts found in the config file'
-	echo 'Visit https://linuxize.com/post/using-the-ssh-config-file/ for further information'
-	echo 'Downloading SSH script'
-	wget https://raw.githubusercontent.com/matthieu-rdt/Toolbox/master/Linux/SSH.sh && chmod u+x SSH.sh
-	exit 4
-fi
-
-sudo apt list rsync
-if	[ $? -eq 1 ] ; then
-	echo 'Installing rsync'
-	sudo apt-get install rsync -y
-fi
+check_ssh_config
 
 for file in "${sync_important_files[@]}" ; do
 	ssh $remote_host "if ! [ -d $file ] ; then mkdir -p $(dirname $file) ; fi"
 	ssh $remote_host cat $file | diff - $file
 	if [ $? -eq 1 ] ; then
-		echo 'Changes found at $(date "+%T") on $(date "+%A %d %B %Y")'
+		echo "Changes found at $(date "+%T") on $(date "+%A %d %B %Y")"
 		sync_all_files
 	fi
 done
+
+ssh $remote_host "$(< $script_path)"
