56ac787b0c5dd5b29f1d4fe0d53a0266023593b6
Matthieu <matt.doff@gmail.com>
2022-11-25 13:44:33 +0100

    Added simple quotes to second sed

--- Backup/make_changes_after_rsync.sh
+++ Backup/make_changes_after_rsync.sh
@@ -21,7 +21,7 @@ for file in "${sync_important_files[@]}" ; do
 	ip=$(ip a | grep ens1[0-9].* | grep inet | cut -d'/' -f1 | cut -d' ' -f6)
 	grep -q "NodeAddress=$ip:10051" "$file"
 	if	[ $? -eq 1 ] ; then
-		sed -i s/NodeAddress=.*/NodeAddress=$ip:10051/g "$file"
+		sed -i 's/NodeAddress=.*/NodeAddress='$ip':10051/g' "$file"
 	fi
 
 done
