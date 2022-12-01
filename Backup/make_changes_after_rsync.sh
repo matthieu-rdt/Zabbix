4109449d9dde1bef1063325cde27c6d4c8c0fcc4
Matthieu <matt.doff@gmail.com>
2022-12-01 12:53:26 +0100

    Added ^ to filter better

--- Backup/make_changes_after_rsync.sh
+++ Backup/make_changes_after_rsync.sh
@@ -10,13 +10,13 @@ for file in "${sync_important_files[@]}" ; do
 
 	grep -q "HANodeName=$(hostname)" "$file"
 	if	[ $? -eq 1 ] ; then
-		sed -i 's/HANodeName=.*/HANodeName='$(hostname)'/g' "$file"
+		sed -i 's/^HANodeName=.*/HANodeName='$(hostname)'/g' "$file"
 	fi
 
 	ip=$(ip a | grep ens1[0-9].* | grep inet | cut -d'/' -f1 | cut -d' ' -f6)
 	grep -q "NodeAddress=$ip:10051" "$file"
 	if	[ $? -eq 1 ] ; then
-		sed -i 's/NodeAddress=.*/NodeAddress='$ip':10051/g' "$file"
+		sed -i 's/^NodeAddress=.*/NodeAddress='$ip':10051/g' "$file"
 	fi
 
 done
