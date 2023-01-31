21613de519f058d165e746229c41588213a2137c
Matthieu <67316829+matthieu-rdt@users.noreply.github.com>
2023-01-31 19:21:55 +0100

    Fix 'zabbix_backup.sql.gz' filename

--- Upgrade/upgrade_zabbix_server_to_6.sh
+++ Upgrade/upgrade_zabbix_server_to_6.sh
@@ -53,7 +53,7 @@ backup_zabbix_files ()
 
 backup_zabbix_database ()
 {
-	sudo mysqldump -h localhost -u'backup' -p$backup_password --single-transaction $database_name | gzip > ~/zabbix_backup.sql
+	sudo mysqldump -h localhost -u'backup' -p$backup_password --single-transaction $database_name | gzip > ~/zabbix_backup.sql.gz
 	sudo mv zabbix_backup.sql /opt/zabbix_backup/db_files/
 }
 
