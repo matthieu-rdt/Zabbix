ccebee3f2884f0a012da39b51ef3bfbc11c2d99e
Matthieu <matt.doff@gmail.com>
2023-03-24 13:25:17 +0100

    'case' instead of if/else'

--- Add-ons/Database/configure_new_disk.sh
+++ Add-ons/Database/configure_new_disk.sh
@@ -12,15 +12,16 @@ configure_new_disk ()
 	sudo cfdisk /dev/$block
 
 	read -p 'Which FS type (ext4 or xfs) ' type
-	if 	[ $type == ext4 ] ; then
+	case $type in
+		"ext4")
 		# '1' because cfdisk created 1 partition
 		sudo mkfs.ext4 /dev/$block'1'
 		blkid=`sudo blkid | grep $block | awk '{ print $2 }'`
 		read -p 'Give your disk a name ' name
 		echo "# $name is on /dev/$block" 				| sudo tee -a /etc/fstab
-		echo "$blkid $FOLDER ext4 defaults,errors=remount-ro 0 2 	| sudo tee -a /etc/fstab
+		echo "$blkid $FOLDER ext4 defaults,errors=remount-ro 0 2 	| sudo tee -a /etc/fstab;;
 
-	elif	[ $type == xfs ] ; then
+		"xfs")
 		sudo apt install xfsprogs -y
 		sudo modprobe -v xfs
 		# '1' because cfdisk created 1 partition
@@ -28,10 +29,11 @@ configure_new_disk ()
 		blkid=`sudo blkid | grep $block | awk '{ print $2 }'`
 		read -p 'Give your disk a name ' name
 		echo "# $name is on /dev/$block" 				| sudo tee -a /etc/fstab
-		echo "$blkid $FOLDER xfs defaults,errors=remount-ro 0 2 	| sudo tee -a /etc/fstab
-	else
-		exit 3
-	fi
+		echo "$blkid $FOLDER xfs defaults,errors=remount-ro 0 2 	| sudo tee -a /etc/fstab;;
+
+		*)
+		exit 3;;
+	esac
 
 	sudo mount -a
 }
