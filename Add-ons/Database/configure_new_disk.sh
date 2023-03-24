e652175d6be7e61edca1dd0350333732e92318f4
Matthieu <matt.doff@gmail.com>
2023-03-24 14:20:35 +0100

    'case' fixed

--- Add-ons/Database/configure_new_disk.sh
+++ Add-ons/Database/configure_new_disk.sh
@@ -14,21 +14,21 @@ configure_new_disk ()
 	read -p 'Which FS type (ext4 or xfs) ' type
 	case $type in
 		"ext4")
-		# '1' because cfdisk created 1 partition
-		sudo mkfs.ext4 /dev/$block'1'
-		blkid=`sudo blkid | grep $block | awk '{ print $2 }'`
-		read -p 'Give your disk a name ' name
-		echo "# $name is on /dev/$block" 				| sudo tee -a /etc/fstab
+		# '1' because cfdisk created 1 partition;
+		sudo mkfs.ext4 /dev/$block'1';
+		blkid=`sudo blkid | grep $block | awk '{ print $2 }'`;
+		read -p 'Give your disk a name ' name;
+		echo "# $name is on /dev/$block" 				| sudo tee -a /etc/fstab;
 		echo "$blkid $FOLDER ext4 defaults,errors=remount-ro 0 2 	| sudo tee -a /etc/fstab;;
 
 		"xfs")
-		sudo apt install xfsprogs -y
-		sudo modprobe -v xfs
-		# '1' because cfdisk created 1 partition
-		sudo mkfs.xfs /dev/$block'1'
-		blkid=`sudo blkid | grep $block | awk '{ print $2 }'`
-		read -p 'Give your disk a name ' name
-		echo "# $name is on /dev/$block" 				| sudo tee -a /etc/fstab
+		sudo apt install xfsprogs -y;
+		sudo modprobe -v xfs;
+		# '1' because cfdisk created 1 partition;
+		sudo mkfs.xfs /dev/$block'1';
+		blkid=`sudo blkid | grep $block | awk '{ print $2 }'`;
+		read -p 'Give your disk a name ' name;
+		echo "# $name is on /dev/$block" 				| sudo tee -a /etc/fstab;
 		echo "$blkid $FOLDER xfs defaults,errors=remount-ro 0 2 	| sudo tee -a /etc/fstab;;
 
 		*)
