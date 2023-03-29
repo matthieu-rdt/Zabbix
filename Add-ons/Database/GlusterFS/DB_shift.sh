590b98b21c6d562d8a70aa2ecd1a1c0bf8b41586
Matthieu <matt.doff@gmail.com>
2023-03-29 14:52:39 +0200

    Second revision

--- Add-ons/Database/GlusterFS/DB_shift.sh
+++ Add-ons/Database/GlusterFS/DB_shift.sh
@@ -25,62 +25,35 @@ ZABBIX_NODE=""
 # New DB host
 IPADDR=""
 
-#-----------------------#
-#	Functions	#
-#-----------------------#
-
-whoami_root ()
-{
-	if	[ `whoami` = root ] ; then
-		$1
-	else
-		sudo $1
-	fi
-}
-
 #-------------------#
 #	Start	    #
 #-------------------#
 
-egrep --quiet '=""$' $0
+egrep -q '=""$' $0
 if      [ $? -eq 0 ] ; then
-echo "Variables are empty"
-exit 3
+	echo "Variables are empty"
+	exit 3
 fi
 
 grep -q $ZABBIX_NODE ~/.ssh/config
 if	[ $? -eq 0 ] ; then
 	echo "Stop MariaDB & Zabbix server services on $ZABBIX_NODE" ; sleep 2
-	if [ `whoami` = root ] ; then
-		ssh $ZABBIX_NODE 'systemctl stop {zabbix-server,mariadb}.service'
-	else
-		ssh $ZABBIX_NODE 'sudo systemctl stop {zabbix-server,mariadb}.service'
-	fi
+	ssh $ZABBIX_NODE 'sudo systemctl stop {zabbix-server,mariadb}.service'
 else
 	echo "$ZABBIX_NODE or file do not exist"
 	exit 2
 fi
 
 # localhost will host the DB
-sed -i 's/^DBHost=.*/DBHost='$IPADDR'/' $backend
-sed -i '5s/= '.*'/= \x27'$IPADDR'\x27\;/' $frontend
+sudo sed -i 's/^DBHost=.*/DBHost='$IPADDR'/' $backend
+sudo sed -i '5s/= '.*'/= \x27'$IPADDR'\x27\;/' $frontend
 
 # remote host to connect to DB
-ssh $ZABBIX_NODE "sed -i 's/^DBHost=.*/DBHost=$IPADDR/'" $backend
-ssh $ZABBIX_NODE "sed -i '5s/= '.*'/= \x27$IPADDR\x27;/'" $frontend
+ssh $ZABBIX_NODE "sudo sed -i 's/^DBHost=.*/DBHost=$IPADDR/'" $backend
+ssh $ZABBIX_NODE "sudo sed -i '5s/= '.*'/= \x27$IPADDR\x27;/'" $frontend
 
-echo "Start MariaDB service on localhost" ; sleep 2
-if      [ `whoami` = root ] ; then
-	systemctl start mariadb.service
-else
-	sudo systemctl start mariadb.service
-fi
+echo "Start MariaDB & Zabbix server services on localhost" ; sleep 2
+sudo systemctl start {mariadb,zabbix-server}.service
 
-echo "Restart Zabbix server service" ; sleep 2
-if [ `whoami` = root ] ; then
-	systemctl start zabbix-server.service
-	ssh $ZABBIX_NODE 'systemctl start zabbix-server.service'
-else
-	sudo systemctl start zabbix-server.service
-	ssh $ZABBIX_NODE 'sudo systemctl start zabbix-server.service'
-fi
+echo "Start Zabbix server services on $ZABBIX_NODE" ; sleep 2
+ssh $ZABBIX_NODE 'sudo systemctl start zabbix-server.service'
