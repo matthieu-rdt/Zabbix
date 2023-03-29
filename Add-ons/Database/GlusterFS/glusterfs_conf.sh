fdf1e7a7db3ce56c9c312ab2f5e3eb3042a46be1
Matthieu <matt.doff@gmail.com>
2023-03-29 16:58:12 +0200

    First revision

--- Add-ons/Database/GlusterFS/glusterfs_conf.sh
+++ Add-ons/Database/GlusterFS/glusterfs_conf.sh
@@ -11,25 +11,56 @@
 #       Variables       #
 #-----------------------#
 
-
+NODE1=""
+NODE2=""
+DEFAULT_MOUNTPOINT="/data/glusterfs"
+VOLUME=""
 
 #-----------------------#
 #	Functions	#
 #-----------------------#
 
-
+ConfirmChoice ()
+{
+	ConfYorN="";
+	while [ "${ConfYorN}" != "y" ] && [ "${ConfYorN}" != "Y" ] && [ "${ConfYorN}" != "n" ] && [ "${ConfYorN}" != "N" ]
+	do
+		echo -n "$1" "(y/n) : "
+		read ConfYorN
+	done
+	[ "${ConfYorN}" == "y" ] || [ "${ConfYorN}" == "Y" ] && return 0 || return 1
+}
 
 #-----------------#
 #      Start      #
 #-----------------#
 
-grep -q "`hostname`" /etc/hosts && grep -q "$NODE2" /etc/hosts ; echo $?
+grep -q $NODE1 /etc/hosts && grep -q $NODE2 /etc/hosts ; echo $?
+if	[ $? -eq 1 ] ; then
+	echo 'Configure /etc/hosts by adding your cluster nodes'
+	exit 2
+fi
 
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install glusterfs-server -y ; systemctl enable --now glusterd
+sudo apt-get install glusterfs-server -y ; sudo systemctl enable --now glusterd
 
-gluster peer probe $NODE2
+ConfirmChoice "Are you configuring the primary node ?" && \
+gluster peer probe $NODE2 ; gluster peer status ; sleep 3
 
-gluster peer status ; sleep 5
+ConfirmChoice "Create default mountpoint ($DEFAULT_MOUNTPOINT) ?" && mkdir -p $DEFAULT_MOUNTPOINT || \
+read -p 'Enter mountpoint path ' DEFAULT_MOUNTPOINT
+mkdir -p $DEFAULT_MOUNTPOINT
 
-mkdir $
+ConfirmChoice "Are you configuring the primary node ?" && \
+gluster volume create $VOLUME replica 2 $NODE1:/$DEFAULT_MOUNTPOINT $NODE2:/$DEFAULT_MOUNTPOINT \
+echo "Starting volume $VOLUME" \
+gluster volume start $VOLUME
+
+# Create replication point
+read -p 'Enter replication path ' REPLICATION
+mkdir -p $REPLICATION
+
+# Auto mount
+ConfirmChoice "Are you configuring the primary node ?" && \
+echo "$NODE2:/$VOLUME $REPLICATION glusterfs defaults,_netdev 0 0" | sudo tee -a /etc/fstab
+echo "$NODE1:/$VOLUME $REPLICATION glusterfs defaults,_netdev 0 0" | sudo tee -a /etc/fstab
