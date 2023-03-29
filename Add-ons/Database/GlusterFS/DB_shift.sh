4bb69ffc04f3b011805eff269a75a6f66ab60ba1
Matthieu <matt.doff@gmail.com>
2023-03-29 14:26:15 +0200

    First revision

--- Add-ons/Database/GlusterFS/DB_shift.sh
+++ Add-ons/Database/GlusterFS/DB_shift.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # description
-# script is specific to reconfiguring Zabbix server instance after a shift between replicated databases
-# script is specific to using glusterfs
+# script is specific to
+# - reconfiguring Zabbix server instance after a shift between replicated databases
+# - using Zabbix HA cluster and/or glusterfs
 
 # note
 # SSH connection is required between nodes (~/.ssh/config)
@@ -24,23 +25,36 @@ ZABBIX_NODE=""
 # New DB host
 IPADDR=""
 
+#-----------------------#
+#	Functions	#
+#-----------------------#
+
+whoami_root ()
+{
+	if	[ `whoami` = root ] ; then
+		$1
+	else
+		sudo $1
+	fi
+}
+
 #-------------------#
 #	Start	    #
 #-------------------#
 
-grep
-if	[ $? -eq 0 ] ; then
-	echo "Variables are empty"
-	exit 3
+egrep --quiet '=""$' $0
+if      [ $? -eq 0 ] ; then
+echo "Variables are empty"
+exit 3
 fi
 
 grep -q $ZABBIX_NODE ~/.ssh/config
 if	[ $? -eq 0 ] ; then
-	echo "Stop MariaDB service on $ZABBIX_NODE" ; sleep 2
+	echo "Stop MariaDB & Zabbix server services on $ZABBIX_NODE" ; sleep 2
 	if [ `whoami` = root ] ; then
-		ssh $ZABBIX_NODE 'systemctl stop mariadb.service'
+		ssh $ZABBIX_NODE 'systemctl stop {zabbix-server,mariadb}.service'
 	else
-		ssh $ZABBIX_NODE 'sudo systemctl stop mariadb.service'
+		ssh $ZABBIX_NODE 'sudo systemctl stop {zabbix-server,mariadb}.service'
 	fi
 else
 	echo "$ZABBIX_NODE or file do not exist"
@@ -56,13 +70,17 @@ ssh $ZABBIX_NODE "sed -i 's/^DBHost=.*/DBHost=$IPADDR/'" $backend
 ssh $ZABBIX_NODE "sed -i '5s/= '.*'/= \x27$IPADDR\x27;/'" $frontend
 
 echo "Start MariaDB service on localhost" ; sleep 2
-sudo systemctl start mariadb.service
+if      [ `whoami` = root ] ; then
+	systemctl start mariadb.service
+else
+	sudo systemctl start mariadb.service
+fi
 
 echo "Restart Zabbix server service" ; sleep 2
 if [ `whoami` = root ] ; then
-	systemctl restart zabbix-server.service ; sleep 3
-	ssh $ZABBIX_NODE 'systemctl restart zabbix-server.service'
+	systemctl start zabbix-server.service
+	ssh $ZABBIX_NODE 'systemctl start zabbix-server.service'
 else
-	sudo systemctl restart zabbix-server.service ; sleep 3
-	ssh $ZABBIX_NODE 'sudo systemctl restart zabbix-server.service'
+	sudo systemctl start zabbix-server.service
+	ssh $ZABBIX_NODE 'sudo systemctl start zabbix-server.service'
 fi
