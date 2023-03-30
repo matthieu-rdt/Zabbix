e8d6df1878b4bd597fdee4b3b6b7d01510fa8065
Matthieu <matt.doff@gmail.com>
2023-03-30 16:17:05 +0200

    Second revision

--- Add-ons/Database/GlusterFS/glusterfs_conf.sh
+++ Add-ons/Database/GlusterFS/glusterfs_conf.sh
@@ -3,6 +3,9 @@
 # description
 # setup a database replication by using GlusterFS for Zabbix 6 and greater
 
+# note
+# /!\ script needs to be run on each node at the same time /!\
+
 # sources
 # https://docs.gluster.org/en/v3
 # https://www.digitalocean.com/community/tutorials/how-to-create-a-redundant-storage-pool-using-glusterfs-on-ubuntu-20-04
@@ -13,54 +16,86 @@
 
 NODE1=""
 NODE2=""
-DEFAULT_MOUNTPOINT="/data/glusterfs"
+MOUNTPOINT=""
+REPLICATION=""
 VOLUME=""
 
 #-----------------------#
-#	Functions	#
+#       Functions       #
 #-----------------------#
 
 ConfirmChoice ()
 {
-	ConfYorN="";
-	while [ "${ConfYorN}" != "y" ] && [ "${ConfYorN}" != "Y" ] && [ "${ConfYorN}" != "n" ] && [ "${ConfYorN}" != "N" ]
-	do
-		echo -n "$1" "(y/n) : "
-		read ConfYorN
-	done
-	[ "${ConfYorN}" == "y" ] || [ "${ConfYorN}" == "Y" ] && return 0 || return 1
+        ConfYorN="";
+        while [ "${ConfYorN}" != "y" ] && [ "${ConfYorN}" != "Y" ] && [ "${ConfYorN}" != "n" ] && [ "${ConfYorN}" != "N" ]
+        do
+                echo -n "$1" "(y/n) : "
+                read ConfYorN
+        done
+        [ "${ConfYorN}" == "y" ] || [ "${ConfYorN}" == "Y" ] && return 0 || return 1
 }
 
 #-----------------#
 #      Start      #
 #-----------------#
 
-grep -q $NODE1 /etc/hosts && grep -q $NODE2 /etc/hosts ; echo $?
-if	[ $? -eq 1 ] ; then
-	echo 'Configure /etc/hosts by adding your cluster nodes'
-	exit 2
-fi
+ConfirmChoice "Configure $NODE1 (primary node) ?" &&
+{
+        grep -q $NODE1 /etc/hosts && grep -q $NODE2 /etc/hosts
+        if      [ $? -eq 1 ] ; then
+                echo 'Opening /etc/hosts to add the nodes of your cluster'
+				sudo vi /etc/hosts
+        fi
 
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install glusterfs-server -y ; sudo systemctl enable --now glusterd
+        if !    [ `hostname` = $NODE1 ] ; then
+                echo 'Configuring hostname ...'
+                sudo hostnamectl set-hostname $NODE1
+                loginctl terminate-user `who | awk '{print $1}'`
+        fi
 
-ConfirmChoice "Are you configuring the primary node ?" && \
-gluster peer probe $NODE2 ; gluster peer status ; sleep 3
+        echo 'Installing GlusterFS ... ' && sleep 2
+        sudo apt-get update && sudo apt-get upgrade -y
+        sudo apt-get install glusterfs-server -y ; sudo systemctl enable --now glusterd
+        gluster peer probe $NODE2 ;  sleep 2
 
-ConfirmChoice "Create default mountpoint ($DEFAULT_MOUNTPOINT) ?" && mkdir -p $DEFAULT_MOUNTPOINT || \
-read -p 'Enter mountpoint path ' DEFAULT_MOUNTPOINT
-mkdir -p $DEFAULT_MOUNTPOINT
+        ConfirmChoice "Create mountpoint : $MOUNTPOINT ?" && mkdir -p $MOUNTPOINT
 
-ConfirmChoice "Are you configuring the primary node ?" && \
-gluster volume create $VOLUME replica 2 $NODE1:/$DEFAULT_MOUNTPOINT $NODE2:/$DEFAULT_MOUNTPOINT \
-echo "Starting volume $VOLUME" \
-gluster volume start $VOLUME
+		ConfirmChoice "$MOUNTPOINT must be created on each cluster's node, continue ?" && \
+		gluster volume create $VOLUME replica 2 transport tcp $NODE1:/$MOUNTPOINT $NODE2:/$MOUNTPOINT
+        
+		echo "Starting volume $VOLUME ..." && sleep 2
+        gluster volume start $VOLUME
 
-# Create replication point
-read -p 'Enter replication path ' REPLICATION
-mkdir -p $REPLICATION
+        ConfirmChoice "Create replication point : $REPLICATION ?" && mkdir -p $REPLICATION
 
-# Auto mount
-ConfirmChoice "Are you configuring the primary node ?" && \
-echo "$NODE2:/$VOLUME $REPLICATION glusterfs defaults,_netdev 0 0" | sudo tee -a /etc/fstab
-echo "$NODE1:/$VOLUME $REPLICATION glusterfs defaults,_netdev 0 0" | sudo tee -a /etc/fstab
+        # Auto mount
+        echo "$NODE2:/$VOLUME $REPLICATION glusterfs defaults,_netdev 0 0" | sudo tee -a /etc/fstab
+        ConfirmChoice "Mount all ?" && mount -a
+}
+
+ConfirmChoice "Configure $NODE2 ?" &&
+{
+        grep -q $NODE1 /etc/hosts && grep -q $NODE2 /etc/hosts
+        if      [ $? -eq 1 ] ; then
+                echo 'Opening /etc/hosts to add the nodes of your cluster'
+                sudo vi /etc/hosts
+        fi
+
+        if !    [ `hostname` = $NODE2 ] ; then
+                echo 'Configuring hostname ...'
+                sudo hostnamectl set-hostname $NODE2
+                loginctl terminate-user `who | awk '{print $1}'`
+        fi
+
+        echo 'Installing GlusterFS ... ' && sleep 2
+        sudo apt-get update && sudo apt-get upgrade -y
+        sudo apt-get install glusterfs-server -y ; sudo systemctl enable --now glusterd
+
+        ConfirmChoice "Create mountpoint : $MOUNTPOINT ?" && mkdir -p $MOUNTPOINT
+		
+        ConfirmChoice "Create replication point : $REPLICATION ?" && mkdir -p $REPLICATION
+
+        # Auto mount
+        echo "$NODE1:/$VOLUME $REPLICATION glusterfs defaults,_netdev	0	0" | sudo tee -a /etc/fstab
+        ConfirmChoice "Mount all ?" && mount -a
+}
