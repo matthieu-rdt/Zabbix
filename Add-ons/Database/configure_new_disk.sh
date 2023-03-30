d2ec80049ed90e9fad6ee76d10df61afed831488
Matthieu <matt.doff@gmail.com>
2023-03-30 16:30:19 +0200

    Dealing with FS EXT4

--- Add-ons/Database/configure_new_disk.sh
+++ Add-ons/Database/configure_new_disk.sh
@@ -1,41 +1,39 @@
 #!/bin/bash
 
+# description
+# configure a new disk with EXT4
+# next step : add XFS
+
+#-----------------------#
+#	Functions	#
+#-----------------------#
+
 configure_new_disk ()
 {
-	read -p 'Enter a folder name to create ? (without '/' at the end) ' FOLDER
-	[ ! -d $FOLDER ] && sudo mkdir -p $FOLDER
-
 	# Get the new disk name
-	lsblk ; sleep 2
-	read -p 'Which disk do you want to partition (e.g. sda, sdb) ? ' block
+	lsblk
+
+	read -p "choose the block device you want to use for DB ? " block
+
 	sudo cfdisk /dev/$block
 
-	read -p 'Which FS type (ext4 or xfs) ' type
-	case $type in
-		"ext4")
-		# '1' because cfdisk created 1 partition;
-		sudo mkfs.ext4 /dev/$block'1';
-		blkid=`sudo blkid | grep $block | awk '{ print $2 }'`;
-		read -p 'Give your disk a name ' name;
-		echo "# $name is on /dev/$block" 				| sudo tee -a /etc/fstab;
-		echo "$blkid $FOLDER ext4 defaults,errors=remount-ro 0 2 	| sudo tee -a /etc/fstab;;
+	# '1' because cfdisk created 1 partition
+	sudo mkfs.ext4 /dev/$block'1'
 
-		"xfs")
-		sudo apt install xfsprogs -y;
-		sudo modprobe -v xfs;
-		# '1' because cfdisk created 1 partition;
-		sudo mkfs.xfs /dev/$block'1';
-		blkid=`sudo blkid | grep $block | awk '{ print $2 }'`;
-		read -p 'Give your disk a name ' name;
-		echo "# $name is on /dev/$block" 				| sudo tee -a /etc/fstab;
-		echo "$blkid $FOLDER xfs defaults,errors=remount-ro 0 2 	| sudo tee -a /etc/fstab;;
+	read -p "Enter a path for the mountpoint (without '/' at the end) " mountpoint
+	[ ! -d $mountpoint ] && sudo mkdir -p $mountpoint
 
-		*)
-		exit 3;;
-	esac
+	# Get UUID
+	blkid=`sudo blkid | grep $block | awk '{ print $2 }'`
 
+	# FSTAB
+	echo "# folder is on /dev/$block" | sudo tee -a /etc/fstab
+	echo "$blkid $mountpoint ext4 defaults	0	2" | sudo tee -a /etc/fstab
 	sudo mount -a
 }
 
-echo 'supported FS : ext4 & xfs'
+#-------------------#
+#	Start	    #
+#-------------------#
+
 configure_new_disk
