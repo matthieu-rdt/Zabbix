451a3ea8873fc8254cd0ba88197227fb433c5349
Matthieu <matt.doff@gmail.com>
2023-03-31 20:16:45 +0200

    Fixed minor bugs

--- Add-ons/Database/GlusterFS/DB_switch.sh
+++ Add-ons/Database/GlusterFS/DB_switch.sh
@@ -16,8 +16,8 @@
 #-----------------------#
 
 # FILES
-backend="/etc/zabbix/zabbix_server.conf"
-frontend="/etc/zabbix/web/zabbix.conf.php"
+BACKEND="/etc/zabbix/zabbix_server.conf"
+FRONTEND="/etc/zabbix/web/zabbix.conf.php"
 
 # Current DB host to stop
 ZABBIX_NODE=""
@@ -48,7 +48,8 @@ check_vars ZABBIX_NODE IPADDR
 grep -q $ZABBIX_NODE ~/.ssh/config
 if	[ $? -eq 0 ] ; then
 	echo "Stop MariaDB & Zabbix server services on $ZABBIX_NODE" ; sleep 2
-	ssh $ZABBIX_NODE 'sudo systemctl stop {zabbix-server,mariadb}.service'
+	ssh $ZABBIX_NODE 'sudo systemctl stop zabbix-server.service'
+	ssh $ZABBIX_NODE 'sudo systemctl stop mariadb.service'
 else
 	echo "$ZABBIX_NODE or file do not exist"
 	wget https://raw.githubusercontent.com/matthieu-rdt/Toolbox/master/Linux/SSH.sh && chmod u+x SSH.sh
@@ -57,16 +58,16 @@ else
 fi
 
 # localhost will host the DB
-sudo sed -i 's/^DBHost=.*/DBHost='$IPADDR'/' $backend
-sudo sed -i '5s/= '.*'/= \x27'$IPADDR'\x27\;/' $frontend
+sudo sed -i 's/^DBHost=.*/DBHost='$IPADDR'/' $BACKEND
+sudo sed -i '5s/= '.*'/= \x27'$IPADDR'\x27\;/' $FRONTEND
 
 # remote host to connect to DB
-ssh $ZABBIX_NODE "sudo sed -i 's/^DBHost=.*/DBHost=$IPADDR/'" $backend
-ssh $ZABBIX_NODE "sudo sed -i '5s/= '.*'/= \x27$IPADDR\x27;/'" $frontend
+ssh $ZABBIX_NODE "sudo sed -i 's/^DBHost=.*/DBHost=$IPADDR/'" $BACKEND
+ssh $ZABBIX_NODE "sudo sed -i '5s/= '.*'/= \x27$IPADDR\x27;/'" $FRONTEND
 
 echo "Start MariaDB & Zabbix server services on localhost" ; sleep 2
-sudo systemctl start mariadb
-sudo systemctl restart zabbix-server
+sudo systemctl start mariadb.service
+sudo systemctl start zabbix-server.service
 
 echo "Start Zabbix server services on $ZABBIX_NODE" ; sleep 2
-ssh $ZABBIX_NODE 'sudo systemctl restart zabbix-server.service'
+ssh $ZABBIX_NODE 'sudo systemctl start zabbix-server.service'
