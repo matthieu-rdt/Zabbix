80ffda9ae2fc756f589fa684d65984cc0cd01ec4
Matthieu <matt.doff@gmail.com>
2023-10-31 15:58:04 +0100

    Added prep env

--- Add-ons/PowerShell/installation_powershell_lts.sh
+++ Add-ons/PowerShell/installation_powershell_lts.sh
@@ -15,3 +15,12 @@ echo 'Installing PowerShell'
 wget https://github.com/PowerShell/PowerShell/releases/download/v7.2.14/powershell-lts_7.2.14-1.deb_amd64.deb
 sudo dpkg -i powershell-lts_7.2.14-1.deb_amd64.deb
 sudo apt install powershell-lts
+
+echo 'Downloading Microsoft profile'
+wget https://raw.githubusercontent.com/matthieu-rdt/Zabbix/main/Add-ons/PowerShell/Microsoft.PowerShell_profile.ps1
+cp Microsoft.PowerShell_profile.ps1 -t $HOME/.config/powershell
+#cp Microsoft.PowerShell_profile.ps1 -t /root/.config/powershell
+
+echo 'Preparing environment'
+sudo touch /etc/profile.d/powershell.sh
+echo 'export PWSH_SCRIPTS_DIR="/usr/local/share/powershell/Scripts"' | sudo tee /etc/profile.d/powershell.sh
