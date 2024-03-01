b058d08312fb5eb8d9177eccf090bb2f328efc56
Matthieu <matt.doff@gmail.com>
2024-03-01 15:44:07 +0100

    Download a file instead of cat EOF

--- Add-ons/Vault/configuration_vault.sh
+++ Add-ons/Vault/configuration_vault.sh
@@ -9,6 +9,7 @@
 
 # Variables
 CONFIG_FILE=/etc/vault.d/vault.hcl
+FILE=$HOME/vault.hcl"
 FQDN=$(hostname -f)
 IP_ADDR=""
 STORAGE_PATH=""
@@ -21,50 +22,13 @@ cert_key ()
 
 vault_hcl ()
 {
-	cat << EOF > $CONFIG_FILE
-	# Copyright (c) HashiCorp, Inc.
-	# SPDX-License-Identifier: BUSL-1.1
+	if	[ ! -f "$FILE" ] ; then
+		curl -sO https://raw.githubusercontent.com/matthieu-rdt/Zabbix/main/Add-ons/Vault/vault.hcl
+	fi
 
-	# Full configuration options can be found at https://developer.hashicorp.com/vault/docs/configuration
-
-	# Log Section
-	log_level = "info"
-	log_file = "/var/log/vault.log"
-
-	# Enable Web Interface
-	ui = true
-
-	# Storage Type
-	storage "file" {
-	  path = \""$STORAGE_PATH"\"
-	}
-
-	# HTTPS listener
-	listener "tcp" {
-	  address       = "$IP_ADDR:8200"
-	  tls_cert_file = "/opt/vault/tls/$FQDN.cer"
-	  tls_key_file  = "/opt/vault/tls/$FQDN.key"
-	  tls_disable = false
-	}
-
-	# HA Parameters
-	#api_addr = "https://$FQDN:8200"
-	#cluster_addr = "https://$FQDN:8201"
-
-	## Options
-	# HTTP listener
-	#listener "tcp" {
-	#  address     = "17.18.19.0:8200"
-	#  tls_disable = 1
-	#}
-
-	# RAFT storage
-	#storage "raft" {
-	#  path = "/opt/vault/data"
-	#  node_id = "node_1"
-	#}
-
-	EOF
+	while IFS= read -r line ; do
+	echo $line | sudo tee -a $CONFIG_FILE > /dev/null
+	done < $FILE
 }
 
 permissions ()
