81ab1480050ffc14b08a176cf235469fd8c8af4f
Matthieu <matt.doff@gmail.com>
2024-03-05 12:46:39 +0100

    Simplification : only cert/key + permissions

--- Add-ons/Vault/configuration_vault.sh
+++ Add-ons/Vault/configuration_vault.sh
@@ -9,81 +9,33 @@
 
 # Variables
 FQDN=$(hostname -f)
-IP_ADDR=""
 STORAGE_PATH=""
 
-cert_key ()
-{
-	cp /etc/ssl/certs/$FQDN.cer /opt/vault/tls/
-	cp /etc/ssl/private/$FQDN.key /opt/vault/tls/
-	chmod 600 /opt/vault/tls/*
-	chown -R vault:vault /opt/vault
-}
+systemctl stop vault
 
-vault_hcl () {
-echo "# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: BUSL-1.1
+###################
+# Copy cert & key #
+###################
+cp /etc/ssl/certs/$FQDN.cer /opt/vault/tls/
+cp /etc/ssl/private/$FQDN.key /opt/vault/tls/
 
-# Full configuration options can be found at https://developer.hashicorp.com/vault/docs/configuration
+#########################
+# Configure permissions #
+#########################
+# Cert & Key permissions
+chmod 600 /opt/vault/tls/*
+chown -R vault:vault /opt/vault
 
-# Log Section
-log_level = "info"
-log_file = "/var/log/vault.log"
+# Configuration file permission
+chmod 644 /etc/vault.d/vault.hcl
+chown vault:vault /etc/vault.d/vault.hcl
 
-# Enable Web Interface
-ui = true
+# Log file permission
+touch /var/log/vault.log
+chown vault:vault /var/log/vault.log
 
-# Storage Type
-storage "file" {
-path = \""$STORAGE_PATH"\"
-}
+# Data Storage permission
+chmod 755 $STORAGE_PATH
+chown -R vault:vault $STORAGE_PATH
 
-# HTTPS listener
-listener "tcp" {
-address       = "$IP_ADDR:8200"
-tls_cert_file = "/opt/vault/tls/$FQDN.cer"
-tls_key_file  = "/opt/vault/tls/$FQDN.key"
-tls_disable = false
-}
-
-# HA Parameters
-#api_addr = "https://$FQDN:8200"
-#cluster_addr = "https://$FQDN:8201"
-
-## Options
-# HTTP listener
-#listener "tcp" {
-#  address     = "17.18.19.0:8200"
-#  tls_disable = 1
-#}
-
-# RAFT storage
-#storage "raft" {
-#  path = "/opt/vault/data"
-#  node_id = "node_1"
-#}" | tee /etc/vault.d/vault.hcl }
-
-permissions ()
-{
-	# Log file permission
-	touch /var/log/vault.log
-	chown vault:vault /var/log/vault.log
-
-	# Data Storage permission
-	chmod 755 $STORAGE_PATH
-	chown -R vault:vault $STORAGE_PATH
-}
-
-start_vault ()
-{
-	systemctl start vault
-	echo "Visit https://$FQDN:8200"
-}
-
-cert_key
-
-vault_hcl
-
-permissions
-
-start_vault
+systemctl start vault
